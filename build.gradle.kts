4fe8cae1af93162955b9114327b0d0a46dbfba9c
Cola <vaultslave@gmail.com>
2022-06-24 02:29:21 +0100

    Add maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import net.civmc.civgradle.CivGradleExtension
 plugins {
     `java-library`
     `application`
+    `maven-publish`
     id("net.civmc.civgradle") version "2.+"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
